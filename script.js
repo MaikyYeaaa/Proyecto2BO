f922bb4bdf513814d7fb736d81e32eb2415d23e6
MaikyYeaaa <62185218+MaikyYeaaa@users.noreply.github.com>
2022-11-01 16:41:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function ajustarfooter() {
+    var winH = $(window).height();
+    var contenidoH = $("header").height();
+    
+    if (contenidoH < winH) {
+      $("nav").attr("class", "bg-light");
+        }
+    }
+  
+  $(document).ready(ajustarfooter);
+  $(window).on("resize", ajustarfooter);
